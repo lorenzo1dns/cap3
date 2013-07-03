9f1bf0e01948ac02b9112a9fe67becf5f710412e
lorenzo <lorenzo_dns@yahoo.com>
2013-07-03 10:50:20 -0600

    commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cap3::Application.config.secret_token = '5fee4afbd6f6dcf41933d1f17c2c60d9e751a3831a774836a7c238602c841144fceaa65acf8fe85467db1ac4b30349a388babe92cf6b1fecb0981807319e2aa0'
